0a1d025ecec4cd9947a2b3c4473c2183eb9851ad
Nicolas DE ARRIBA <ndearriba@office.bcstechno.com>
2018-06-11 13:42:45 +0200

    test

--- main.tf
+++ main.tf
@@ -1,22 +1,18 @@
 provider "azurerm" {}
+
 variable "prefix" {
   default = "mon"
 }
 
 resource "azurerm_resource_group" "tf-rg" {
-   name = "rg${var.prefix}"
-   location = "West US"
+  name     = "rg${var.prefix}"
+  location = "West US"
 }
 
 resource "azurerm_virtual_network" "MyResource" {
-   name = "virtualNetwork1${var.prefix}"
-   location = "${azurerm_resource_group.tf-rg.location}"
-   resource_group_name = "${azurerm_resource_group.MyResource.name}"
-   address_space = ["10.0.0.0/16"]
-   dns_servers = ["10.0.0.4"]
+  name                = "virtualNetwork1${var.prefix}"
+  location            = "${azurerm_resource_group.tf-rg.location}"
+  resource_group_name = "${azurerm_resource_group.MyResource.name}"
+  address_space       = ["10.0.0.0/16"]
+  dns_servers         = ["10.0.0.4"]
 }
-
-tf-azurerm
-
-
-test
